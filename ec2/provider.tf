c83aec3a1ee9fda6f3a04565e5f7a7b807486db9
NormaBuendia <normabdv@gmail.com>
2024-06-27 11:56:02 -0400

    cambios

--- ec2/provider.tf
+++ ec2/provider.tf
@@ -1,13 +1,13 @@
 # Terraform provider configuration
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "5.54.1"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.54.1"
+    }
+  }
+}
 
-# provider "aws" {
-#   region = var.region
-# }
+provider "aws" {
+  region = var.region
+}
