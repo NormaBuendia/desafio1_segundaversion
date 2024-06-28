e59dc39ff3484aaab7bfacaf83787c3a3d77e068
NormaBuendia <normabdv@gmail.com>
2024-06-28 11:47:14 -0400

    se coloco provider configuration

---
+++ ec2/provider.tf
@@ -0,0 +1,9 @@
+# Terraform provider configuration
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.54.1"
+    }
+  }
+}
--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,6 +1,5 @@
 #variable region donde se va adesplegar la EC2 instance
 variable "region" {
-    
     type = string
     description = " The AWS region to deploy to "
     default= "us-east-1"
