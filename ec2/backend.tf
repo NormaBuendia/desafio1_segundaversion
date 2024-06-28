2922d86966b4f5d957a8eec99e4e015bbbbafa0c
NormaBuendia <normabdv@gmail.com>
2024-06-28 10:40:43 -0400

    se borrarron archivos

--- ec2/backend.tf
+++
@@ -1,8 +0,0 @@
-# Terraform backend configuration de bucket creado en aws
-# terraform {
-#   backend "s3" {
-#     bucket = "nombre de bucket creado en aws"
-#     key    = "nginx-ec2/terraform.tfstate"
-#     region = "us-east-1"
-#   }
-# }
